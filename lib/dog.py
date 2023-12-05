41ef013d4146c569a8326d2b2e03cc3ab55d5a8b
Matthew Bomett <bomettmat2@gmail.com>
2023-12-05 19:18:48 +0300

    Completed Assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 class Dog:
+    def __init__(self, name, breed='Mutt'):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print(f'Woof! My name is {self.name} and I am a {self.breed}')
     pass
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def talk(self):
+        print(f'Hello! My name is {self.name}')
+
+    def walk(self):
+        print(f'{self.name} is walking')
     pass
